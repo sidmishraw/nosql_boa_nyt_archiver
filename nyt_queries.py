42b0a7f7a436290f4526e6fd834c791a1f794b8c
Sidharth Mishra <sidmishraw@gmail.com>
2017-05-06 13:42:32 -0700

    added allowDiskUse to aggregate queries

--- nyt_queries.py
+++ nyt_queries.py
@@ -3,7 +3,7 @@
 # @Author: Sidharth Mishra
 # @Date:   2017-03-15 12:36:16
 # @Last Modified by:   Sidharth Mishra
-# @Last Modified time: 2017-05-06 12:48:56
+# @Last Modified time: 2017-05-06 13:42:04
 
 
 '''
@@ -1347,13 +1347,13 @@ def count_original_articles():
 
   cursor = db[__COLLECTION_NAME__].find(query)
 
-  count = 0
+  # count = 0
 
-  while cursor is not None:
-    count += 1
-    cursor = cursor.next()
+  # while cursor is not None:
+  #   count += 1
+  #   cursor = cursor.next()
 
-  org_articles_count = count  # len(list(cursor)) if cursor is not None else 0
+  org_articles_count = len(list(cursor)) if cursor is not None else 0
 
   return org_articles_count
 
